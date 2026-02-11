ceabc846e1d918f6832f63fed0af41ea0eaa9499
mitsonani <mitsonani7777@gmail.com>
2026-02-11 17:01:20 +0530

    refactor: remove unused `lucide-react` icons and `Button` component import from Footer.

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,6 +1,4 @@
-import { Instagram, Twitter, Facebook, ArrowRight } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import { Button } from '../components/ui/Primitives';
 
 const FOOTER_LINKS = [
     {
